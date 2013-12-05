1f872169955e432b34650701e47caf8350f3a25e
Harrison Powers <fijimunkii@gmail.com>
2013-12-05 12:02:35 -0500

    linked list class

---
+++ lib/linked_list.rb
@@ -0,0 +1,11 @@
+class LinkedList
+
+  def initialize
+    @root = nil
+  end
+
+  def root
+    @root
+  end
+
+end
---
+++ lib/main.rb
@@ -0,0 +1,7 @@
+require_relative 'node'
+require_relative 'linked_list'
+
+list = LinkedList.new
+list.unshift("A")
+list.unshift("B")
+list.unshift("C")
